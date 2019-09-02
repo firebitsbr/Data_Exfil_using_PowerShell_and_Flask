11b448f6333e9b636aec3b8c9221cde69bb08040
Ashish Khairnar <ashish.khairnar1@gmail.com>
2019-09-02 11:37:20 +0800

    Create client.ps1

---
+++ client.ps1
@@ -0,0 +1,27 @@
+$URI = Read-Host 'Enter URL: '
+
+$File_path = Read-Host 'Enter File Path: '
+
+Write-Host "Invoke PowerShell web request"
+
+try{
+
+    $sess = Invoke-WebRequest -Method POST -URI $URI -Infile $File_path
+    
+    if($sess.StatusCode -eq 200){
+            write-host("Success!")
+    }
+    
+    else {
+            write-host("Something went wrong")
+} 
+
+    Write-Host "done"
+
+    }
+
+catch{
+
+    Write-Host "Failed!!!"
+
+}
